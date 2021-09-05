a5433f260c7e39341744fa93ab5b80c6dd452005
showkat-ALI <cpianshowkat@gmail.com>
2021-09-05 22:14:28 +0600

    storage section added

---
+++ sessionalStorage-VS-localStorage.js
@@ -0,0 +1,5 @@
+/*Basically the main work of each storage is to store data. But there are a little bit of differece of them. The data have been saved in to local storage will remain ultill the catch of the browser have been cleard. But the data will be vanished of the sessional storage if be close the tab.  */
+
+/* the data we got from each storage is in json format . if we want to read data we have to parse it . if we want to store data as object we have keep it as stringyfy */
+/* localStorage and sessionStorage are almost identical and have the same API. The difference is that with sessionStorage , the data is persisted only until the window or tab is closed. With localStorage , the data is persisted until the user manually clears the browser cache or until your web app clears the data.Dec 28, 2016
+ */

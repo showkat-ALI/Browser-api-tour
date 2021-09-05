431e2b2c34da0e400a7649271f050bdd6cc7628d
showkat-ALI <cpianshowkat@gmail.com>
2021-09-05 18:44:15 +0600

    history api details added

---
+++ historyApi.js
@@ -0,0 +1,8 @@
+/* with the help of history api we can go forward or backward page .
+Basically its just a api which we use store our history. There are some methods of this object.
+There are some usefull method of this object these are::
+
+history.back()
+history.forward()
+history.go()
+ */
